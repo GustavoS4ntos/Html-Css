2796d64c91b3b643e00ac6c284fa539adfaee1f3
Gustavo Santos <playkyng@gmail.com>
2023-03-20 16:30:48 -0300

    att

---
+++ Desafios/CriarFuncao/index.js
@@ -0,0 +1,72 @@
+
+
+ //1 
+
+
+// function exiberMsgTeste(){
+
+//     console.log("Olá Mundo");
+    
+//     }
+//     exiberMsgTeste();
+
+
+
+// 2 
+
+
+// function exibirNome(nome){
+
+// console.log (`Meu Nome é: ${nome}`);
+
+// }
+
+// exibirNome("Gustavo");
+
+//3 
+
+// function exibirInfo(nome, idade , estilo){
+
+// console.log(`Olá Meu nome é ${nome} eu tenho ${idade} anos e curto ${estilo}`);
+// }
+
+// exibirInfo("Gustavo","24","Sertanejo");
+
+//4
+
+// function exibirGosto (filme, musica){
+// console.log(`Olá eu gosto muito do filme ${filme} e da musica ${musica}`);
+
+// }
+
+// exibirGosto("Matrix","Para Sempre")
+
+//5 
+
+// function triplo (numero){
+
+// console.log (numero * 3)
+
+// }
+
+// triplo (2);
+
+//6 
+
+function verificar(valor){
+
+
+if (valor > 10){ console.log("Valor é 5")
+
+
+}else{
+
+console.log ("valor é 20")
+
+}
+
+}
+
+verificar(11);
+
+
