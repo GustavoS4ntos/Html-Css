303aa0cad87f5ace13d4266d439886bcb87026c6
Gustavo Santos <playkyng@gmail.com>
2023-03-20 18:13:08 -0300

    att

---
+++ Desafios/Arrays/script.js
@@ -0,0 +1,102 @@
+// //1
+
+// let frutas = [`Banana`,`Maçã`,`Uva`, `Pera`,`Goiaba`];
+
+// console.log(frutas.length);
+// console.log(frutas)
+
+// //2 
+
+// let adicionar = frutas.unshift("Morango");
+// console.log(frutas)
+
+// //3 
+
+// let remover = frutas.pop();
+// console.log(frutas)
+
+// //4 
+
+// let adicionarFinal = frutas.push("Melão","Acelora");
+// console.log(frutas);
+
+// //5
+
+// let removerPrimeiro = frutas.shift();
+// console.log(frutas);
+
+// //6 
+
+// let numbers = [7,5,6,3,8,9,2,1,4]
+// let organizar = numbers.sort();
+// console.log(numbers);
+
+//7
+
+// let sobreMim = new Object();
+
+// sobreMim.idade = "24";
+// sobreMim.musicaFavorita = "Para Sempre";
+// sobreMim.ano = "1999";
+
+// //8
+
+// sobreMim.nome = "Gustavo";
+
+
+
+// // 9
+
+// delete sobreMim.idade
+
+
+// //10 
+
+// console.log(sobreMim);
+
+//11
+
+
+let cadrasto = [{
+
+
+nome: "Gustavo",
+idade: "24",
+telefone:"17996790180",
+amigos : ["Deus","Deus","Deus","Deus"]
+
+},{
+
+
+    nome: "Adriano",
+    idade: "24",
+    telefone:"17996790180",
+    amigos: ["Deus","Deus","Deus","Deus"]
+    },{
+
+
+        nome: "Maicon",
+        idade: "24",
+        telefone:"17996790180",
+        amigos: ["Deus","Deus","Deus","Deus"]
+        },{
+
+
+            nome: "Fernando",
+            idade: "24",
+            telefone:"17996790180",
+            amigos: ["Deus","Deus","Deus","Deus"]
+            
+            },{
+
+
+                nome: "Gustavo",
+                idade: "24",
+                telefone:"17996790180",
+                amigos : ["Deus","Deus","Deus","Deus"]
+                }]
+
+
+                console.log(cadrasto[1]);
+
+
